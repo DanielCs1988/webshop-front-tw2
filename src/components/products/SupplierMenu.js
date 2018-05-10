e3798d648c667f721adbde2f880ee4831e9e4f91
DanielCs88 <Pg26cv%g37>
2018-05-10 17:06:07 +0200

    Fixed menus.

--- src/components/products/SupplierMenu.js
+++ src/components/products/SupplierMenu.js
@@ -5,44 +5,29 @@ class SupplierMenu extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            suppliersToRender: [],
-            suppliersToRenderId: [],
-            categoryId: null
+            suppliers: []
         };
-        this.getSupplier();
-        this.state.categoryId = this.props.hoveringCategoryId;
-
-
     }
 
-    getSupplier() {
-        this.setState({'suppliersToRender': []});
-        for (let supplier of this.props.supplierList) {
-            for (let id of this.props.hoveringCategorySuppliersId){
-                if (id === supplier.id){
-                    this.state.suppliersToRender.push(supplier.name);
-                    this.state.suppliersToRenderId.push(supplier.id);
-                }
-            }
-        }
+    componentDidMount(){
+        fetch('/webshop/supplier')
+            .then(result => result.json())
+            .then(jsonResult => this.setState({ suppliers: jsonResult }));
     }
 
     render(){
-        const suppliers = this.state.suppliersToRender.map((item, i) => {
+        const suppliers = this.state.suppliers.map(item => {
             return (
                 <div className="menu-list-item" key={item.id} >
-                    <h5 className="effect-shine" onClick={() => {
-                        this.props.applyFilter(this.state.suppliersToRenderId[i], this.state.categoryId)
-                    }}>
-                        {item}
+                    <h5 className="effect-shine" onClick={() => this.props.applySupplierFilter(item.id)}>
+                        {item.name}
                     </h5>
                 </div>
             );
         });
 
         return (
-            <div onMouseEnter={() => {this.props.handleMouseHoverUl()}}
-                 onMouseLeave={() => {this.props.handleMouseHoverUl()}} className="supplier-menu">
+            <div className="supplier-menu">
                 <ul className="list-group">
                     {suppliers}
                 </ul>
